ca6f17253fb4d5cfd277723ea26dd754312d3315
БлиновС <@233_14>
2018-04-04 19:45:27 +0600

    module_6

--- Bankomat/Program.cs
+++ Bankomat/Program.cs
@@ -12,6 +12,53 @@ namespace Bankomat
     {
         static void Main(string[] args)
         {
+            try
+            {
+                Console.Write("Enter yuor login: ");
+                string login = Console.ReadLine();
+                Console.Write("Enter your password: ");
+                string password = Console.ReadLine();
+
+                if (login == "admin" && password == "admin")
+                {
+                    Client client = new Client();
+
+                    Service.createClient(ref client);
+
+                    client.Login = "admin";
+                    client.Password = "admin";
+
+                    Console.Clear();
+                    Console.WriteLine("1. Список счетов");
+                    Console.WriteLine("2. Создать счет");
+
+                    int choise = 0;
+
+                    Int32.TryParse(Console.ReadLine(), out choise);
+
+                    if (choise > 2 || choise < 1)
+                    {
+                        throw new Exception("Invalid choise");
+                    }
+                    else
+                    {
+                        switch (choise)
+                        {
+                            case 1: { } break;
+                            case 2: { } break;
+                        }
+                    }
+                }
+                else
+                {
+                    throw new Exception("Login or password ERROR");
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
             List<Client> ListClient = new List<Client>();
 
             GeneratorName.Generator g= new Generator();
---
+++ Bankomat/Service.cs
@@ -0,0 +1,22 @@
+﻿using KKB.Bank.Module;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GeneratorName;
+
+namespace Bankomat
+{
+    class Service
+    {
+        public static void createClient(ref Client client)
+        {
+            Generator gen = new Generator();
+            client.FullName = gen.GenerateDefault(Gender.woman);
+            client.IIN = "860905300951";
+            client.PhoneNumber = "7772798927";
+            client.DoB = DateTime.Now;
+        }
+    }
+}
