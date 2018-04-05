a2a0e228b3a201feaa9341c3aec556c7bf8e0dc2
shalun002 <32149071+shalun002@users.noreply.github.com>
2018-04-05 11:11:54 +0600

    module_6

--- Bankomat/Program.cs
+++ Bankomat/Program.cs
@@ -18,118 +18,99 @@ namespace Bankomat
             try
             {
                 Client client = new Client();
-
                 Service.createClient(ref client);
-
                 client.Login = "admin";
                 client.Password = "admin";
 
                 while (!client.isBlocked)
                 {
-                    Console.Clear();
+                    #region
 
-                    Console.Write("Введите логин ");
-                    login = Console.ReadLine(); ;
-                    Console.Write("Введите пароль ");
+                    Console.Clear();
+                    Console.Write("введите логин:");
+                    login = Console.ReadLine();
+                    Console.Write("введите пароль:");
                     password = Console.ReadLine();
 
                     if (login != client.Login && password != client.Password)
                         client.WrongField++;
                     else
                         break;
+                    #endregion
                 }
-
-
-                //Console.Clear();
-
-                //Console.WriteLine("Menu");
-                //Console.WriteLine("Inter login");
-                //Login = Console.ReadLine(); ;
-                //Console.WriteLine("Inter Password");
-                //Password = Console.ReadLine();
 
                 if (login == client.Login && password == client.Password)
                 {
+                    #region
                     if (client.isBlocked)
                     {
-                        Console.WriteLine("You arr blocked");
+                        Console.WriteLine("Пользователь заблокирован!");
                     }
                     else
                     {
-                        #region r
-                        Console.Clear();
-                        Console.WriteLine(" 1 - Список счетов");
-                        Console.WriteLine(" 2 - Создать счет");
-                        Console.WriteLine(" 3 - Пополнить счет");
-
-                        int menu = 0;
-                        Int32.TryParse(Console.ReadLine(), out menu);
-
-                        if (menu > 6 || menu < 1)
+                        #region 
+                        do
                         {
-                            throw new Exception("Нет такого пункта меню");
-                        }
-                        else
-                        {
-                            switch (menu)
+                            Console.Clear();
+
+                            Console.WriteLine("1. Список счетов");
+                            Console.WriteLine("2. Создать счет");
+                            Console.WriteLine("3. Пополнить счёт");
+
+                            Console.WriteLine("6. Выход");
+
+
+                            int menu = 0;
+                            Int32.TryParse(Console.ReadLine(), out menu);
+                            if (menu > 6 || menu < 1)
                             {
-                                // список счетов
-                                case 1:
-                                    {
-                                        Console.Clear();
-                                        client.PrintAccountInfo();
-                                    }
-                                    break;
-                                    // создать счет
-                                case 2:
-                                    {
-                                        Account acc = Service.createAccount();
-                                        client.ListAccount.Add(acc);
-                                        Console.WriteLine("Счет добавлен успешно!");
-                                    }
-                                    break;
-                                case 3:
-                                    {
-                                        Console.Write("Введите номер счета: ");
-                                        string accountNumber = Console.ReadLine();
-                                        Console.Write("Введите сумму: ");
-                                        string accountSumm = Console.ReadLine();
-                                    }
-                                    break;
-                                case 6:
-                                    return;
+                                throw new Exception("Нет такого пункта в меню");
                             }
-                        }
+                            else
+                            {
+                                switch (menu)
+                                {
+                                    //Список счетов
+                                    case 1:
+                                        {
+                                            client.PrintAccountInfo();
+                                        }
+                                        break;
+                                    //Создать счет
+                                    case 2:
+                                        {
+                                            Account acc = Service.createAccount();
+                                            client.ListAccount.Add(acc);
+                                            Console.WriteLine("Счёт добавлен успешно!");
+                                        }
+                                        break;
+                                    case 3:
+                                        {
+                                            Console.WriteLine("Введите номер счёта: ");
+                                            string accountNumber = Console.ReadLine();
+                                            Console.WriteLine("Введите сумму ");
+                                            string acconutSum = Console.ReadLine();
+                                        }
+                                        break;
+                                    case 6:
+                                        return;
+                                }
+                            }
+                        } while (true);
                         #endregion
                     }
+                    #endregion
                 }
                 else
-                    Console.WriteLine("You arr blocked");
-
+                {
+                    Console.Clear();
+                    Console.WriteLine("User blocked");
+                }
             }
-            catch (Exception)
+            catch (Exception ex)
             {
-
-                throw;
+                Console.WriteLine(ex.Message);
             }
         }
-
-        //List<Client> ListClient = new List<Client>();
-
-        //GeneratorName.Generator g= new Generator();
-
-        //Client c1 = new Client();
-
-        //c1.DoB = DateTime.Now.AddYears(-60);
-        //c1.FullName = g.GenerateDefault(Gender.man);
-        //c1.IIN = "970131301448";
-        //c1.Login = "Qwe";
-        //c1.Password = "123";
-        //c1.PhoneNumber = "87475458546";
-
-        //ListClient.Add(c1);
-
-        //c1.ClientInfoPrint();
     }
 }
-
